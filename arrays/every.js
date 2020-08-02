e1895771aab00b6f762adcbda814a502a251eeb8
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-08-02 23:10:16 +0200

    Adds fill & corrects every

--- arrays/every.js
+++ arrays/every.js
@@ -5,9 +5,7 @@ const iterator = myArray.entries();
 const isEven = (currentValue) => currentValue % 2 === 0;
 const executor = () => {
     console.log('Description: Array.every(): tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.');
-    console.log('', myArray.every(isEven));
-    console.log(iterator.next().value);
-    console.log(iterator.next());
+    console.log('Is element in array even?', myArray.every(isEven));
 };
 
 executor();
