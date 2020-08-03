cadc99fba7d6d98d36e652562efd2c1e510da02a
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-08-03 19:21:38 +0200

    Adds find func

---
+++ arrays/find.js
@@ -0,0 +1,10 @@
+// The find() method returns the value of the first element in the provided array that satisfies the provided testing function.
+myArray = require('../helpers');
+const isEven = (currentValue) => currentValue % 2 === 0;
+
+const executor = () => {
+    console.log('Description: Array.find(): immutable - returns the value of the first element in a new array from the original array that returns true against the function its given.');
+    console.log('Finds the first even number from given array & returns just that item', myArray.find(number => isEven(number)));
+};
+
+executor();
