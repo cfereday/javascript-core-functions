3c39e7f00a1fe22d8cf44f4d0313b8fed38a8b3b
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-08-03 23:45:41 +0200

    Adds flat fn

---
+++ arrays/flat.js
@@ -0,0 +1,12 @@
+myArray = require('../helpers');
+const isEven = (currentValue) => currentValue % 2 === 0;
+
+const myNestedArray = [myArray, [myArray]];
+
+const executor = () => {
+    console.log('myNestedArray', myNestedArray);
+    console.log('Description: Array.flat(): immutable - returns a new array with any nested arrays concatenated into it for a single level array. It also removes empty slots in arrays');
+    console.log('My flattened array - only flattens down one level', myNestedArray.flat());
+};
+
+executor();
