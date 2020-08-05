93632ba130805acf892905b6465dc2b8833727eb
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-08-05 23:09:48 +0200

    Adds indexOf fn

---
+++ arrays/indexOf.js
@@ -0,0 +1,8 @@
+myArray = require('../helpers');
+
+const executor = () => {
+    console.log('Description: Array.indexOf(): immutable because it returns an index number: method returns the first index at which a given element can be found in the array, or -1 if it is not present.');
+    console.log('Example result: the index of the number 1 in my array', myArray.indexOf(1));
+};
+
+executor();
