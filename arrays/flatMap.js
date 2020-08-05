1913c67585eae7d7489743374e4282eb1efdc357
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-08-05 16:02:49 +0200

    Adds flatMap fn

---
+++ arrays/flatMap.js
@@ -0,0 +1,9 @@
+myArray = require('../helpers');
+const isEven = (currentValue) => currentValue % 2 === 0;
+
+const executor = () => {
+    console.log('Description: Array.flatMap(): immutable - returns a new array with the results of applying map for a test function & then flattening  the result in a new result. Exactly the same as map() chained after with a flat() of depth 1 - but bit more efficient');
+    console.log('My flattened array - only flattens down one level', myNestedArray.flat());
+};
+
+executor();
