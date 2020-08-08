5668af803fa4511e0f6832334bc33955fbdbee5f
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-08-08 14:43:46 +0200

    Adds slice fn

---
+++ arrays/slice.js
@@ -0,0 +1,10 @@
+myArray = require('../helpers');
+
+const executor = () => {
+    console.log('Description: Array.slice(): immutable - returns a new array which is a shallow copy of a portion of the array: give it start index & end index - end is not included' );
+    console.log('My original array:', myArray);
+    console.log('Example result: slice with 0, 4 args:', myArray.slice(0,4));
+    console.log('Example result: slice with 2 arg:', myArray.slice(2));
+};
+
+executor();
