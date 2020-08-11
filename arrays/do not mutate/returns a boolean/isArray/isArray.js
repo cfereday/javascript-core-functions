7d4c4f8869d451199af2a9b4cb42be01ccd702c2
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-08-11 22:56:00 +0200

    Adds sketch & mini rename

--- arrays/do not mutate/returns a boolean/isArray/isArray.js
+++ arrays/do not mutate/returns a boolean/isArray/isArray.js
@@ -1,7 +1,7 @@
-myArray = require('../../../helpers');
+myArray = require('../../../../helpers');
 
 const executor = () => {
-    console.log('Description: Array.isArray(): immutable because it returns a boolean to say if its an array or not');
+    console.log('Description: Array.isArray(): immutable because it returns a boolean to say if the thing it is passed is an array or not');
     console.log('The original array:', myArray);
     console.log('An example array: is', Array.isArray(myArray));
     console.log('An example non array: is', Array.isArray({foo: 123}));
