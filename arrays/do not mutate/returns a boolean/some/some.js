6a133cf0ec0556e7a8ef399177f4fd58c1b99d5c
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-08-12 21:50:34 +0200

    Adds skecth for some

--- arrays/do not mutate/returns a boolean/some/some.js
+++ arrays/do not mutate/returns a boolean/some/some.js
@@ -1,9 +1,9 @@
-myArray = require('../../../helpers');
+myArray = require('../../../../helpers');
 
 const isEven = (element) => element % 2 === 0;
 
 const executor = () => {
-    console.log('Description: Array.some(): immutable as returns a boolean value - tests whether at least one element in the array returns true for the test fn' );
+    console.log('Description: Array.some(): immutable as returns a boolean value - tests whether at least one element in the array matches the criteria for the test fn' );
     console.log('My original array:', myArray);
     console.log('Example result: is there an element that is even in the array?:', myArray.some(isEven));
 };
